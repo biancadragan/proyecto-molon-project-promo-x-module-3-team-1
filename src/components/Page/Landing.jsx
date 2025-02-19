ec8dde1c7b67e5584f6c538dc0eea207c4d846c0
biancadragan <andreeabiancadragan@gmail.com>
2025-02-19 19:52:31 +0100

    Add image to landing page

--- src/components/Page/Landing.jsx
+++ src/components/Page/Landing.jsx
@@ -1,9 +1,16 @@
 import Hero from "../Layout/Hero";
 import { Link } from "react-router";
+import landingImage from "../../images/cool-project.png";
+
 function Landing() {
   return (
     <>
       <section className="main hero-complex">
+      <img
+        src={landingImage}
+        alt="Imagen de la landing"
+        className="landingImage"
+      />
         <Hero>
           <Link className="button--link" to="/MainPage">
             Nuevo proyecto
